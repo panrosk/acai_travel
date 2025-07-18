2d5535225e5ce9e5f6eeb8b495a6ad344ca0e88a
Oscar <oskrf13@gmail.com>
2025-07-18 11:45:58 +0200

    Initial commit

---
+++ internal/server/server.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type FiberServer struct {
+	*fiber.App
+}
+
+func New() *FiberServer {
+	server := &FiberServer{
+		App: fiber.New(fiber.Config{
+			ServerHeader: "acai_travel",
+			AppName:      "acai_travel",
+		}),
+	}
+
+	return server
+}
